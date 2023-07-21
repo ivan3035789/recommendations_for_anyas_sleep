9ea4a4f7d81a909731d1865fc3799fba5a343028
ivan <82367614+ivan3035789@users.noreply.github.com>
2023-07-21 12:47:40 +0300

    update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+a = int(input())
+b = int(input())
+h = int(input())
+if a <= 24 and a <= h <= b and h <= 24:
+    print('Это нормально')
+elif 24 >= a >= h and h < b and h <= 24:
+    print('Недосып')
+elif a <= 24 and h >= a and b < h <= 24:
+    print('Пересып')
